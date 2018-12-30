195736b853e9484a121616e0ce1c623a4600ba53
CharlesZheng <846743326@qq.com>
2018-12-30 14:52:53 +0800

    first commit

---
+++ tf/src/main/java/edu/charles/tengf/TengfApplication.java
@@ -0,0 +1,14 @@
+package edu.charles.tengf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TengfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TengfApplication.class, args);
+    }
+
+}
+
