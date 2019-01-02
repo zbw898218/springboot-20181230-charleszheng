85344f033fdd6cf1746f3dfd69b9bb48ad0c3ae2
CharlesZheng <846743326@qq.com>
2019-01-02 16:14:08 +0800

    更新

--- edu_charles.sql
+++ edu_charles.sql
@@ -10,10 +10,50 @@ Target Server Type    : MYSQL
 Target Server Version : 50722
 File Encoding         : 65001
 
-Date: 2018-12-30 17:07:50
+Date: 2019-01-02 16:13:34
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article` (
+  `id` bigint(20) NOT NULL,
+  `author_id` bigint(20) NOT NULL,
+  `tag_id` bigint(20) NOT NULL,
+  `title` varchar(127) NOT NULL,
+  `desc` varchar(255) DEFAULT NULL,
+  `content` varchar(9999) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of article
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for author
+-- ----------------------------
+DROP TABLE IF EXISTS `author`;
+CREATE TABLE `author` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(63) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `sex` tinyint(4) NOT NULL COMMENT '0:男 1：女',
+  `title` varchar(255) DEFAULT NULL,
+  `description` varchar(1023) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of author
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for tags
@@ -30,4 +70,23 @@ CREATE TABLE `tags` (
 
 -- ----------------------------
 -- Records of tags
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(63) DEFAULT NULL,
+  `sex` tinyint(4) NOT NULL COMMENT '0 男 1女',
+  `age` int(11) DEFAULT NULL,
+  `description` varchar(300) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of user
 -- ----------------------------
